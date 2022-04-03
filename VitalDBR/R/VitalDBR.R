42e3152088d20419da319185524604e52fbe24c3
Georg <au677731@uni.au.dk>
2022-04-03 18:54:53 +0200

    Added decsriptions to functions. Also i removed some functionality, as we no longer want to be able to load multiple tracks from one case. Since the nature of the data does not allow os to combine two timeseries in one DF

--- VitalDBR/R/VitalDBR.R
+++ VitalDBR/R/VitalDBR.R
@@ -1,13 +1,6 @@
-# Brugeren inputter én eller flere tnames i load_case, f.eks. c(BIS/EMG, Primus/INSP_DES, Solar8000/BT)
-# Brugeren inputter også ét (og kun ét) case id. f.eks. "17"
-# Funktionen samler nu alle "tids" forbundet med de forskellige tnames i case 1. ( i det her tilfælde 3 stk.)
-# Funktionen kalder nu load_trks(tids), der kalder load_trk(tid) som kalder load vdb (der henter fra api) på alle tid i tids
-# dermed bliver der returnet 3 tidsserier c(BIS/EMG, Primus/INSP_DES, Solar8000/BT), der alle tilhører case 17
-
-
 #' Loads file from URL
 #' @export
-#' @param file_url URL for the VitablDB website
+#' @param file_url URL for API endpoint
 load_VDB <- function(file_url) {
   con <- gzcon(url(file_url))
   txt <- readLines(con)
@@ -16,7 +9,7 @@ load_VDB <- function(file_url) {
 
 #' Loads a data track from VitalDB api
 #' @export
-#' @param tid PENDING EXPLANATION OF VARIABLE
+#' @param tid Track id
 load_trk <- function(tid){
   start <- "https://api.vitaldb.net/"
   end <- ".csv.gz"
@@ -26,30 +19,14 @@ load_trk <- function(tid){
   return(df)
 }
 
-#' PENDING DESCRIPTION OF FUNCTION
+#' Load a track belonging to a case
 #' @export
-#' @param tids PENDING EXPLANATION OF VARIABLE
-load_trks <- function(tids){
-  data <- data.frame()
-  index <- 1
-  for (i in 1:nrow(tids)){
-    load_trk(tids[i,])
-    index <- index+1
-  }
-}
-
-#' PENDING DESCRIPTION OF FUNCTION
-#' @export
-#' @param tnames PENDING EXPLANATION OF VARIABLE
-#' @param caseid PENDING EXPLANATION OF VARIABLE
-load_case <- function(tnames, caseid){
+#' @param tname Name of track
+#' @param caseid A case id, from which you want to load the track given as tname
+load_case <- function(tname, caseid){
   tracks <- load_VDB("https://api.vitaldb.net/trks.csv.gz")
   tracks <- tracks[tracks$caseid == caseid,]
-  tids <- data.frame(col = rep(NA, length(tnames)))
-  index <- 1
-  for (tname in tnames){
-    tids$col[index] <- (tracks[tracks$tname == tname,])$tid
-    index <- index+1
-  }
-  load_trks(tids)
+  tid <- (tracks[tracks$tname == tname,])$tid
+  load_trk(tid)
 }
+
