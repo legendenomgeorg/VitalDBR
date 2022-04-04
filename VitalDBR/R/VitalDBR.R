4dc3b54573f86d9032de189600763051c269922e
Georg <au677731@uni.au.dk>
2022-04-04 16:06:28 +0200

    Finally the data seems to be in the format we want

--- VitalDBR/R/VitalDBR.R
+++ VitalDBR/R/VitalDBR.R
@@ -7,6 +7,31 @@ load_VDB <- function(file_url) {
   return(read.csv(textConnection(txt)))
 }
 
+#' Small helper function that converts index into hz
+#' @export
+#' @param x a string with an integer
+#' @param freq the frequency of your dataset
+fix_hz_index <- function(x,freq) {
+  return(as.numeric(x)*freq)
+}
+
+#' Checks whether we are importing something that is measured in hz. If it is, it changes the row names to reflect the frequency
+#' @export
+#' @param df The dataframe we want to check. If
+check_hz <- function(df){
+  if (is.na(df[1,2]) & is.na(df[2,2] & !is.na(df[1,1]) & !is.na(df[2,1]) )){
+    freq = df[2,1] # picking out the frequency of the dataset
+    df <- subset (df, select = -Time) #Removing time column
+    df<- na.omit(df) # Removing NA's
+    rownames(df)<- NULL # resetting index to start at 1
+    rownames(df) <- lapply(rownames(df), fix_hz_index, freq=freq) # changing the index into hz
+    return(df)
+  }
+  else {
+    return(df)
+  }
+}
+
 #' Loads a data track from VitalDB api (helper function)
 #' @export
 #' @param tid Track id
@@ -16,7 +41,7 @@ load_trk <- function(tid){
   # start + tid + end
   url <- paste(start, tid, end, sep="")
   df = load_VDB(url)
-  return(df)
+  return(check_hz(df))
 }
 
 #' Main function for loading in data
