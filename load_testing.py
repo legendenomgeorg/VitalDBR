bc82e0835a61d1af8777dab16f095e31c5c72ef5
Georg <au677731@uni.au.dk>
2022-04-04 17:13:34 +0200

    Cleanup of the repo and added a test folder for unit testing

--- load_testing.py
+++
@@ -1,7 +0,0 @@
-#testing vital db load
-import vitaldb
-import pandas as pd
-
-
-df = vitaldb.load_trk("afd182c102c5af625d3f217280b3766d453d9e3f", interval=10)
-print([i for i in df])
---
+++ tests/test_load.R
@@ -0,0 +1,5 @@
+source("../VitalDBR/R/VitalDBR.R", chdir = TRUE)
+library(testthat)
+
+
+# her skal vi skrive nogle unit tests
